ca7ecd8ec9d670220effa917caafb97e6ebc3590
Dolat Hamza <dolathamza8019@gmail.com>
2021-11-23 22:32:18 +0500

    Update assignments.js

--- models/assignments.js
+++ models/assignments.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 var assignmentSchema = new Schema({
-    totalMarks: {
+    TMarks: {
         type: Number,
         required: true,
     },
